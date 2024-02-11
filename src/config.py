6fa7c142b4258f942f62c1bb8931498c1e5d64ea
Rubya Afrin <afrin.ku11@gmail.com>
2024-02-10 22:04:52 -0700

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,12 +1,10 @@
 import os
-RAW_DATA_PATH = r'MIMIC_III_merged.csv' 
+RAW_DATA_PATH = r'dataset/MIMIC_III_merged.csv' 
 
 SAVE_PROCESSED_JSON = False
 
-IMAGE_SAVE_DIR = r'dataset/class_1'
+IMAGE_SAVE_DIR = r'dataset/image'
 os.makedirs(IMAGE_SAVE_DIR, exist_ok=True)
 
 IMAGE_ROW_SIZE = 10
 
-SPLITTED_IMAGE_DIR = r'dataset/split'
-os.makedirs(SPLITTED_IMAGE_DIR, exist_ok=True)
